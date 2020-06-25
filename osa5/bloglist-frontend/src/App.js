a042d68acfa9e98cf5ff30c325e11d3bb434991b
Aleksi Heinimäki <aleksi.heinimaki1@gmail.com>
2020-06-25 13:41:55 +0300

    osa5 alku

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const loginHandler = (e) => {
+    e.preventDefault()
+    console.log('logging', username, password)
+  }
+
+  const loginForm = () => (
+    <div>
+      <h1>log in to application</h1>
+      <form onSubmit={loginHandler}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>      
+    </div>
+  )
+  
+  const blogForm = () => (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+  return (
+    <div>
+      {user === null ?
+        loginForm() :
+        blogForm()
+      }
+
+    </div>
+  )
+}
+
+export default App
